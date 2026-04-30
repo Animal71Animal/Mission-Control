e308d2ea73c6f296f7b21bf32c9f0925116f19a6
Eric Mills <ericmills71@gmail.com>
2026-04-30 04:50:17 +0000

    feat: add second Featured Entertainer video link

--- app/spearmint-rhino/amanda-email/page.tsx
+++ app/spearmint-rhino/amanda-email/page.tsx
@@ -40,7 +40,9 @@ We discussed doing a "Midnight Showtime" where I introduce the girls, and I beli
 
 With my background, I like to make a big theatrical production for showtime that really gets people excited. I've attached a few clips of shows I've produced — the "Midnight Showtime" clips show the intro format, and the "Featured Entertainer" clips show the fully produced performances with custom lighting, music beds, and special effects. This is the kind of value that keeps customers coming back and bringing friends. These are older videos, but they'd give you a sense of what I'm talking about. I'd obviously create new ones for the Spearmint brand at a much higher level with the tools I have now.
 
-Featured Entertainer example: https://drive.google.com/file/d/1RH7hNaDjRQSRrU_jBRa7MsIOszRnmEjC/view?usp=drive_link
+Featured Entertainer examples:
+• https://drive.google.com/file/d/1RH7hNaDjRQSRrU_jBRa7MsIOszRnmEjC/view?usp=drive_link
+• https://drive.google.com/file/d/1hTK1icTTDXhixmLzxftBCNZxlBTcQsfr/view?usp=sharing
 
 EQUIPMENT & SUPPORT REQUESTS
 
